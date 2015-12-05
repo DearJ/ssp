4a4a7cbaf8c11e1a6b0d719f121ef8e89f3ab0b7
guima <1770421128@qq.com>
2015-12-05 21:22:43 +0800

    第一次

---
+++ sptoo/ZLDownLoader.h
@@ -0,0 +1,13 @@
+//
+//  ZLDownLoader.h
+//  podTextLibrary
+//
+//  Created by 鬼马 on 15/12/4.
+//  Copyright © 2015年 guima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZLDownLoader : NSObject
+
+@end
---
+++ sptoo/ZLDownLoader.m
@@ -0,0 +1,21 @@
+//
+//  ZLDownLoader.m
+//  podTextLibrary
+//
+//  Created by 鬼马 on 15/12/4.
+//  Copyright © 2015年 guima. All rights reserved.
+//
+
+#import "ZLDownLoader.h"
+
+@implementation ZLDownLoader
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"测试")；
+    }
+    return self;
+}
+@end
